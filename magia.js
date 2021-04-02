20d170936bd9b219709d441e25e6b6bfb9b57aed
Iver Lopez <44891366+IverLopez@users.noreply.github.com>
2021-04-02 15:50:00 -0400

    Update magia.js

--- magia.js
+++ magia.js
@@ -1,9 +1,1 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-6PZP0XS2HX"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-6PZP0XS2HX');
-</script>
